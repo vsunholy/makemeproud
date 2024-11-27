3183b94a14de2f9390ef65ea40e4095ca8f65702
vsunholy <vitalijsnopov@gmail.com>
2024-11-27 13:39:25 +0200

    11/27/2024

---
+++ 023/public/briedis.js
@@ -0,0 +1,28 @@
+
+
+fetch('http://localhost:3000/briedis')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const h1 = document.createElement('h1');
+        h1.textContent = data.hello;
+        document.body.appendChild(h1);
+
+    })
+
+
+const param = document.querySelector('#param');
+const color = param.querySelector('[type="color"]');
+const range = param.querySelector('[type="range"]');
+
+param.querySelector('button').addEventListener('click', _ => {
+    const colorWithoutHash = color.value.slice(1);
+    fetch(`http://localhost:3000/briedis/${colorWithoutHash}/${range.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const h1 = document.querySelector('h1');
+            h1.style.color = '#' + data.spalva;
+            h1.style.fontSize = data.dydis + 'px';
+        });
+});
--- 023/server.js
+++ 023/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
- 
+
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,27 +12,48 @@ app.get('/', (req, res) => {
 
   res.send('<h1>Hello!</h1>')
 });
-app.get('/barsukas', (req, res) => {
 
+app.get('/barsukas', (req, res) => {
   res.send('<h1>Hello Barsukas!</h1>')
 });
+
+app.get('/briedis', (req, res)=>{
+  const data = {
+    hello: 'Hello, Briedis!'
+  };
+  res.json(data);
+})
 app.get('/barsukas/:color/:size', (req, res) => {
   // const color = req.params.color;
   // const size = req.params.size;
   const { color, size } = req.params;
   res.send(`<h1 style="color:${color};font-size: ${size}px;">Hello Barsukas!</h1>`)
 });
+app.get('/briedis/:color/:size', (req, res) => {
+
+  const { color, size } = req.params;
+  
+  const data = { 
+    spalva:color,
+    dydis: size
+  }
+  res.json(data)
+});
+
 app.get('/bebras', (req, res) => {
   const { color, size } = req.query;
   res.send(`<h1 style="color: ${color ?? 'black'}; font-size: ${size ?? 20}px;">Hello Bebras!</h1>`)
 });
 
 app.post('/form', (req, res) => {
-  const { name, surname, email, password } = req.body;
-  res.send(`Form post received! ${name} ${surname} ${email} ${password}`);
+  // const { name, surname, email, password } = req.body;
+  // res.send(`Form post received! ${name} ${surname} ${email} ${password}`);
+  res.status(302).redirect('http://localhost:3000/success')
 });
 
-
+app.get('/success',(req, res) =>{
+  res.send('<h1>Forma gavome!</h1>');
+})
 
 
 app.listen(port, () => {
