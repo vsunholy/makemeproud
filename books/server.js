29eab546a375359415cad7cb0ef814bd1706229b
vsunholy <vitalijsnopov@gmail.com>
2024-12-03 13:35:04 +0200

    12/3/2024

--- books/server.js
+++ books/server.js
@@ -5,20 +5,49 @@ const fs = require('node:fs');
 const app = express();
 const port = 80;
 
-
+const domain = 'http://books.final';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+const top = fs.readFileSync('./html/top.html', 'utf8');
+const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
+
+
+
+
+
+
+
+
+
+
+
 //ROUTES
 app.get('/create', (req, res) => {
-  const file = fs.readFileSync('./html/create.html', 'utf8');
+  const file = top + fs.readFileSync('./html/create.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
-  const data = { manoKintamasis: 'Bebras valgo' };
+  const data = {
+    manoKintamasis: 'Bebras valgo',
+    pageTitle: 'Nauja knyga',
+    domain: domain
+  };
   const html = template(data);
   res.send(html);
 
 });
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Knygynas darbui pasiruoses ant ${port} porto!`)
