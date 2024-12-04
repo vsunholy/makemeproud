a2ade3da7a30e8d1b3db7c846f81de3add0aafc9
vsunholy <vitalijsnopov@gmail.com>
2024-12-04 13:40:45 +0200

    12/4/2024

--- books/server.js
+++ books/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const handlebars = require('handlebars');
 const fs = require('node:fs');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 80;
 
@@ -16,15 +17,23 @@ const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
 
 
 
-
-
-
-
-
-
-
-
 //ROUTES
+
+app.get('/', (req, res) => {
+  let books = fs.readFileSync('./data/books.json', 'utf8');
+  books = JSON.parse(books);
+  const file = top + fs.readFileSync('./html/read.html', 'utf8') + bottom;
+  const template = handlebars.compile(file);
+  const data = {
+
+    pageTitle: 'Knygu sarasas',
+    domain,
+    books
+  };
+  const html = template(data);
+  res.send(html);
+});
+
 app.get('/create', (req, res) => {
   const file = top + fs.readFileSync('./html/create.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
@@ -38,8 +47,23 @@ app.get('/create', (req, res) => {
 
 });
 
+app.post('/store', (req, res) => {
 
+  const { title, author, year, genre, isbn, pages } = req.body;
+  const id = uuidv4();
+  // need validation
 
+  const book = { id, title, author, year, genre, isbn, pages };
+
+  let data = fs.readFileSync('./data/books.json', 'utf8');
+  data = JSON.parse(data);
+  data.push(book);
+  data = JSON.stringify(data);
+  fs.writeFileSync('./data/books.json', data);
+
+  res.status(302).redirect(domain);
+
+});
 
 
 
