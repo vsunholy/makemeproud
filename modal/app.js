5d6a8e144e6b0e54b107077831c76dbe5768641a
vsunholy <vitalijsnopov@gmail.com>
2024-12-10 15:18:44 +0200

    12 10pt2

---
+++ modal/app.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('button');
+const close = document.querySelector('.close');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modalContent')
+
+
+btn.addEventListener('click', openModal)
+close.addEventListener('click', closeModal)
+
+function openModal() {
+    modal.style.display = 'block'
+}
+
+function closeModal() {
+    modalContent.classList.add('slideUp')
+
+
+    setTimeout(() => {
+        modal.style.display = 'none';
+        modalContent.classList.remove('slideUp');
+    }, 500)
+}
