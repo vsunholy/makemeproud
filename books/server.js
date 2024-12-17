309ec5ab677133e1b858fd6765da5c8f55047372
vsunholy <vitalijsnopov@gmail.com>
2024-12-17 13:44:58 +0200

    12 17

--- books/server.js
+++ books/server.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const handlebars = require('handlebars');
 const multer = require('multer');
 const fs = require('node:fs');
 const { v4: uuidv4 } = require('uuid');
+const { log } = require('handlebars/runtime');
 const app = express();
-const port = 80;
-
 
 handlebars.registerHelper('isdefined', function (value) {
   return value !== undefined;
 });
-
-
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -25,29 +22,76 @@ const storage = multer.diskStorage({
     const filename = `${randomPrefix}.${extension}`;
     cb(null, filename);
   }
+
 });
 
 
 
-
-
-
-
+const port = 80;
 const domain = 'http://books.final/';
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 const top = fs.readFileSync('./html/top.html', 'utf8');
 const bottom = fs.readFileSync('./html/bottom.html', 'utf8');
-
-// MESSAGES
-
 const messages = {
   create_success: { msg: 'Knyga sėkmingai sukurta!', type: 'success' },
   edit_success: { msg: 'Knyga sėkmingai atnaujinta!', type: 'success' },
-  delete_success: { msg: 'Knyga sėkmingai istrinta!', type: 'success' },
-  validation_error: { msg: 'Užpildykite visus laukus!', type: 'danger' }
+  delete_success: { msg: 'Knyga sėkmingai ištrinta!', type: 'success' },
+  validation_error: { msg: 'Užpildykite visus laukus!', type: 'danger' },
+  file_error: { msg: 'Netinkamas paveikslėlio formatas. Palaikomi formatai: jpeg, png', type: 'danger' }
 };
+
+// MIDDLEWARE
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      req.fileValidationError = true;
+    }
+  }
+});
+
+const sessionManager = (req, res, next) => {
+  let sessionId = req.cookies.session || '';
+  let sessionData = fs.readFileSync('./data/session.json', 'utf8');
+  sessionData = JSON.parse(sessionData);
+  const findSession = sessionData.find(s => s.id === sessionId);
+  if (sessionId && findSession) {
+    req.session = structuredClone(findSession);
+  } else {
+    sessionId = uuidv4();
+    const session = { id: sessionId, data: {} };
+    req.session = session;
+    sessionData.push(session);
+    sessionData = JSON.stringify(sessionData);
+    fs.writeFileSync('./data/session.json', sessionData);
+  }
+  res.cookie('session', sessionId, { maxAge: 1000 * 60 * 60 * 24 });
+  next();
+}
+
+const oldDataManager = (req, res, next) => {
+  const requestMethod = req.method;
+  if (requestMethod === 'GET') {
+    addToSession(req, 'oldData', {});
+  } else {
+    addToSession(req, 'oldData', req.body);
+  }
+  next();
+}
+
+app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(upload.single('cover'));
+
+app.use(sessionManager);
+app.use(oldDataManager);
+
+
+// HELPERS
 
 const getMessages = req => {
   if (!req.session.data.msg) return null;
@@ -57,7 +101,6 @@ const getMessages = req => {
   if (!message) return null;
   return message;
 }
-
 
 const addToSession = (req, key, value) => {
   const sessionData = fs.readFileSync('./data/session.json', 'utf8');
@@ -87,42 +130,8 @@ const show404 = res => {
   const html = template(data);
   res.status(404).send(html);
 }
-//* MIDDLEWARE
-const upload = multer({ storage });
 
-const sessionManager = (req, res, next) => {
-  let sessionId = req.cookies.session || '';
-  let sessionData = fs.readFileSync('./data/session.json', 'utf8');
-  sessionData = JSON.parse(sessionData);
-  const findSession = sessionData.find(s => s.id === sessionId);
-  if (sessionId && findSession) {
-    req.session = structuredClone(findSession);
-  } else {
-    sessionId = uuidv4();
-    const session = { id: sessionId, data: {} };
-    req.session = session;
-    sessionData.push(session);
-    sessionData = JSON.stringify(sessionData);
-    fs.writeFileSync('./data/session.json', sessionData);
-  }
-  res.cookie('session', sessionId, { maxAge: 1000 * 60 * 60 * 24 });
-  next();
-}
-
-
-const oldDataManager = (req, res, next) => {
-  const requestMethod = req.method;
-  if (requestMethod === 'GET') {
-    addToSession(req, 'oldData', {});
-  } else {
-    addToSession(req, 'oldData', req.body)
-  }
-  next();
-}
-app.use(sessionManager);
-app.use(oldDataManager);
-
-//* ROUTES
+// ROUTER
 
 app.get('/', (req, res) => {
   // sort
@@ -150,10 +159,6 @@ app.get('/', (req, res) => {
       sortBy = 'default';
   }
 
-
-
-
-
   const file = top + fs.readFileSync('./html/read.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
   const data = {
@@ -162,16 +167,21 @@ app.get('/', (req, res) => {
     books,
     message: getMessages(req),
     sortBy: { [sortBy]: true }
-
   };
+
+  /*
+  const data = {};
+  data.sortBy = 'title_az'; taip noreciau padaryti, kad butu pasirinkta
+  vietoj sitos eilutes:
+  const data = sortBy: {};
+  data.sortBy.title_az = true;
+  */
 
   const html = template(data);
   res.send(html);
-
 });
 
 app.get('/create', (req, res) => {
-
   const file = top + fs.readFileSync('./html/create.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
   const data = {
@@ -181,35 +191,7 @@ app.get('/create', (req, res) => {
     oldData: req.session.data.oldData || {}
   };
   const html = template(data);
-  addToSession(req, 'oldData', {});
   res.send(html);
-
-});
-app.get('/delete/:id', (req, res) => {
-
-  const file = top + fs.readFileSync('./html/delete.html', 'utf8') + bottom;
-  const template = handlebars.compile(file);
-  let books = fs.readFileSync('./data/books.json', 'utf8');
-  books = JSON.parse(books);
-  const id = req.params.id;
-  const book = books.find(book => book.id === id);
-
-  // validation
-  if (!book) {
-    show404(res);
-    return;
-  }
-
-
-  const data = {
-    pageTitle: 'Trynimo patvirtinimas',
-    domain: domain,
-    ...book,
-    nomenu: true
-  };
-  const html = template(data);
-  res.send(html);
-
 });
 
 app.get('/edit/:id', (req, res) => {
@@ -219,12 +201,10 @@ app.get('/edit/:id', (req, res) => {
   books = JSON.parse(books);
   const id = req.params.id;
   const book = books.find(book => book.id === id);
-  // validation
   if (!book) {
     show404(res);
     return;
   }
-
   const data = {
     pageTitle: `Redaguoti knygą ${book.title}`,
     domain: domain,
@@ -234,8 +214,8 @@ app.get('/edit/:id', (req, res) => {
   };
   const html = template(data);
   res.send(html);
-
 });
+
 app.get('/show/:id', (req, res) => {
   const file = top + fs.readFileSync('./html/show.html', 'utf8') + bottom;
   const template = handlebars.compile(file);
@@ -248,53 +228,93 @@ app.get('/show/:id', (req, res) => {
     return;
   }
   const data = {
-    pageTitle: `Rodyti knyga${book.title}`,
+    pageTitle: `Rodyti knygą ${book.title}`,
     domain: domain,
     ...book
   };
   const html = template(data);
   res.send(html);
 });
-app.post('/store', upload.single('cover'), (req, res) => {
+
+app.get('/delete/:id', (req, res) => {
+  const file = top + fs.readFileSync('./html/delete.html', 'utf8') + bottom;
+  const template = handlebars.compile(file);
+  let books = fs.readFileSync('./data/books.json', 'utf8');
+  books = JSON.parse(books);
+  const id = req.params.id;
+  const book = books.find(book => book.id === id);
+  if (!book) {
+    show404(res);
+    return;
+  }
+  const data = {
+    pageTitle: 'Trynimo patvirtinimas',
+    domain: domain,
+    ...book,
+    nomenu: true
+  };
+  const html = template(data);
+  res.send(html);
+});
+
+app.post('/store', (req, res) => {
   const { title, author, year, genre, isbn, pages } = req.body;
+  const uploadFileName = req.file?.filename; // req.file egzistuoja tik jei yra failas
   const id = uuidv4();
-  addToSession(req, 'oldData', req.body);
   if (!title || !author || !year || !genre || !isbn || !pages) {
+    if (uploadFileName) {
+      fs.existsSync(`public/images/${uploadFileName}`) &&
+        fs.unlinkSync(`public/images/${uploadFileName}`); // delete uploaded file
+    }
+
     addToSession(req, 'msg', 'validation_error');
     res.status(422).redirect(domain + 'create');
     return;
   }
-  const uploadFileName = req.file?.filename; // req.file egzistuoja tik jei yra
+  if (req.fileValidationError) {
+    addToSession(req, 'msg', 'file_error');
+    res.status(422).redirect(domain + 'create');
+    return;
+  }
 
-  const book = { id, title, author, year, genre, isbn, pages };
+
+  const book = { id, title, author, year, genre, isbn, pages, cover: uploadFileName };
   let data = fs.readFileSync('./data/books.json', 'utf8');
   data = JSON.parse(data);
   data.push(book);
   data = JSON.stringify(data);
   fs.writeFileSync('./data/books.json', data);
-  addToSession(req, 'msg', 'create_success')
+  addToSession(req, 'msg', 'create_success');
   res.status(302).redirect(domain);
 });
+
 app.post('/update/:id', (req, res) => {
   let books = fs.readFileSync('./data/books.json', 'utf8');
   books = JSON.parse(books);
   const id = req.params.id;
   const oldBook = books.find(book => book.id === id);
-  // validation
   if (!oldBook) {
+    console.log('patekti cia');
     show404(res);
     return;
   }
   const { title, author, year, genre, isbn, pages } = req.body;
-  // need validation
-
+  const uploadFileName = req.file?.filename;
   if (!title || !author || !year || !genre || !isbn || !pages) {
+    if (uploadFileName) {
+      fs.existsSync(`public/images/${uploadFileName}`) &&
+        fs.unlinkSync(`public/images/${uploadFileName}`); // delete uploaded file
+    }
     addToSession(req, 'msg', 'validation_error');
     res.status(422).redirect(domain + 'edit/' + id);
     return;
   }
+  if (req.fileValidationError) {
+    addToSession(req, 'msg', 'file_error');
+    res.status(422).redirect(domain + 'edit/' + id);
+    return;
+  }
 
-  const uploadFileName = req.file?.filename;
   let cover;
 
   if (!uploadFileName) {
@@ -302,12 +322,15 @@ app.post('/update/:id', (req, res) => {
   } else {
     cover = uploadFileName;
   }
-  if (req.body.delete_cover) {
+
+
+  if (req.body.delete_cover && !uploadFileName) {
     cover = undefined; // delete cover entry
   }
- 
+
   if (req.body.delete_cover || uploadFileName) {
-    fs.unlinkSync(`public/images/${oldBook.cover}`); // delete old file
+    fs.existsSync(`public/images/${oldBook.cover}`) &&
+      fs.unlinkSync(`public/images/${oldBook.cover}`); // delete old file
   }
 
 
@@ -318,22 +341,26 @@ app.post('/update/:id', (req, res) => {
   addToSession(req, 'msg', 'edit_success');
   res.status(302).redirect(domain);
 });
+
 app.post('/destroy/:id', (req, res) => {
   let books = fs.readFileSync('./data/books.json', 'utf8');
   books = JSON.parse(books);
   const id = req.params.id;
   const oldBook = books.find(book => book.id === id);
-  // validation
   if (!oldBook) {
     show404(res);
     return;
   }
-  books = books.filter(book => book.id !== id);
+  if (oldBook.cover) {
+    fs.existsSync(`public/images/${oldBook.cover}`) &&
+      fs.unlinkSync(`public/images/${oldBook.cover}`); // delete uploaded file
+  }
+  books = books.filter(book => book.id !== id);oldBook.cover
   books = JSON.stringify(books);
   fs.writeFileSync('./data/books.json', books);
   addToSession(req, 'msg', 'delete_success');
   res.status(302).redirect(domain);
-})
+});
 
 app.listen(port, () => {
   console.log(`Knygynas darbui pasiruošęs ant ${port} porto!`);
